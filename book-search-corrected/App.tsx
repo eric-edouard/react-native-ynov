0e081301421196d167df68c8e33cc2dafefbb8eb
Eric <eric.amilhat@epitech.eu>
2020-11-11 20:17:51 +0100

    removed unused style

--- book-search-corrected/App.tsx
+++ book-search-corrected/App.tsx
@@ -1,7 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import axios from 'axios'
+import React from 'react';
 import MainScreen from './src/screens/MainScreen';
 
 export default function App() {
@@ -9,12 +6,3 @@ export default function App() {
     <MainScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
